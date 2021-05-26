e765560649f089fbf53d81592bfcff8cb1022f93
Yash-Singh1 <saiansh2525@gmail.com>
2021-05-26 12:09:31 -0700

    Use proxy key to remove requirement of CORS ext

--- mastering-redux-and-react/recipefinder/src/helpers.js
+++ mastering-redux-and-react/recipefinder/src/helpers.js
@@ -1,5 +1,5 @@
 export function search(query, cb) {
-  const url = `http://www.recipepuppy.com/api/?i=${query.ingredients}&q=${query.dish}&p=${query.page}`;
+  const url = `/api/?i=${query.ingredients}&q=${query.dish}&p=${query.page}`;
   // console.log('url', url);
   fetch(url)
     .then((response) => response.json())
