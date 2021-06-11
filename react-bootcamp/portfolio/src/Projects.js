b47d4887538f80e4c8e9bfdce6a353d7670bd7aa
Yash-Singh1 <saiansh2525@gmail.com>
2021-06-11 11:16:10 -0700

    Remove console.log

--- react-bootcamp/portfolio/src/Projects.js
+++ react-bootcamp/portfolio/src/Projects.js
@@ -3,8 +3,6 @@ import PROJECTS from './data/projects';
 
 class Project extends Component {
   render() {
-    console.log('this.props', this.props);
-
     const { title, image, description, link } = this.props.project;
 
     return (
